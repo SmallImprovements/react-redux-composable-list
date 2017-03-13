9c77ea5f368b239be0ebe972f197f0aff53788b8
Robin Wieruch <wrobin@gmx.net>
2017-03-13 16:09:25 +0100

    init

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Table = () =>
+  <p>table</p>;
+
+const Row = () =>
+  <p>row</p>;
+
+export default Table;
+
+export {
+  Row
+}
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js',
+    library: 'react-redux-data-grid',
+    libraryTarget: 'commonjs2'
+  },
+  resolve: {
+    root: path.resolve(__dirname),
+    modulesDirectories: ['src', 'node_modules']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+            presets: ['es2015', 'react', 'stage-2']
+        }
+      }
+    ]
+  }
+};
