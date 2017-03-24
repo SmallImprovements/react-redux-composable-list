3ff4583772d8bdcad6198376d5ac2ab92cfa5edc
Robin Wieruch <wrobin@gmx.net>
2017-03-24 11:48:38 +0700

    fix(ducks/reset) array, no object

--- src/ducks/reset/index.js
+++ src/ducks/reset/index.js
@@ -1,8 +1,8 @@
 export const RESET_BY_STATE_KEYS = 'RESET_BY_STATE_KEYS';
 
 export function applyResetByStateKeys(state, { payload }) {
-  const toReset = Object.keys(payload).reduce(payload, (mem, key) => {
-    mem[payload[key]] = undefined;
+  const toReset = payload.reduce((mem, key) => {
+    mem[key] = undefined;
     return mem;
 }, {});
   return { ...state, ...toReset };
